b58a680226d4592bbd546d9c9c61f5b253374299
Benjamin-Pegasus-Fedora <mail.to.benjamin@gmx.de>
2025-01-31 23:52:53 +0100

    Added reverse and dynamic halting

--- traintrack/Scenes/player.gd
+++ traintrack/Scenes/player.gd
@@ -6,10 +6,21 @@ const acceleration = 0.1
 func _physics_process(delta):
 	if Input.is_action_pressed("Train_speed_up") and velocity.z > -speed: #accelerate
 		velocity.z -= acceleration
-	elif Input.is_action_pressed("Train_slow_down") and velocity.z < speed*0.1: #braking
-		velocity.z = lerp(velocity.z,0.0, 0.05)
+	elif Input.is_action_pressed("Train_slow_down"):
+		if velocity.z < speed*0.1: #braking
+			if velocity.z < -0.005:
+				velocity.z = lerp(velocity.z,0.0, 0.05)
+			else:
+				velocity.z = 5
 	else:
 		velocity.z = lerp(velocity.z,0.0, 0.001) #slowed to resistance
+		
+	if Input.is_action_just_released("Train_slow_down") and velocity.z < 0.05:
+		velocity.z = 0
+	elif Input.is_action_just_released("Train_speed_up") and velocity.z > -0.05:
+		velocity.z = 0
+		
+	print(velocity.z)
 
 
 	move_and_slide()
